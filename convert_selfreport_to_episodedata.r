fdd7e4d7c6d5c169c126842f790b411ad859d22e
NIEK VERWEIJ <niekverw@gmail.com>
2020-02-20 13:50:47 +0000

    Update convert_selfreport_to_episodedata.r

--- convert_selfreport_to_episodedata.r
+++ convert_selfreport_to_episodedata.r
@@ -3,42 +3,35 @@ convert_nurseselfreport_to_episodedata <- function(df,field_sr_diagnosis = "2000
   # field_sr_diagnosis = "20002"
   # field_sr_date = "20009"
   # 
+  daysinyear=365.25
   visits = sum(grepl("53_", names(df)))
   
-  df <- df[,c("n_eid","n_34_0_0","n_52_0_0", names(df)[grepl("53_", names(df))],
+  # only need n_eid, visit dates and diag-codes + age-of-diag
+  df <- df[,c("n_eid",names(df)[grepl("53_", names(df))],
               names(df)[grepl(field_sr_diagnosis, names(df))], 
               names(df)[grepl(field_sr_date, names(df))]
   )]
-  daysinyear=365.25
-  # add age and convert visit-date field
-  for (v in 0:(visits-1)){
-    print(v)
-    agefield = paste0("age_",v)
-    visitdatefield = paste0("ts_53_",v,"_0")
-    
-    df[,visitdatefield] <- as.Date (df[,visitdatefield],format="%d%b%Y")
-    df[,agefield] <- as.vector((df[,visitdatefield]- as.Date(paste0(df[,"n_34_0_0"],"/",df[,"n_52_0_0"],"/14"),format = "%Y/%m/%d")) / daysinyear)
-  }
   
   dfout <-  matrix(ncol=3, nrow=0) # initiate output 
   for (v in 0:(visits-1)){ # for each visit, 
+    visitdatefield = paste0("ts_53_",v,"_0")
+    df[,visitdatefield] <- as.Date (df[,visitdatefield],format="%d%b%Y")
     
     diagfields = names(df)[grepl(paste0("n_",field_sr_diagnosis,"_",v),names(df))]
     diagagefields = names(df)[grepl(paste0("n_",field_sr_date,"_",v),names(df))]
-    
+
     for (i in 1:length(diagfields)){ # for each occurence of diagfield, find the corresponding age and convert it to  date - code and rbind() to dfout. 
-      agefield = paste0("age_",v)
-      visitdatefield = paste0("ts_53_",v,"_0")
+      #agefield = paste0("age_",v)
+      
       diagfield = diagfields[i]
       diagagefield = diagagefields[i]
       
       print(paste0((diagfield), "- ", diagagefield))
       
-      dfsub <- df[!is.na(df[,agefield] ) & !is.na(df[,diagfield] ) ,c("n_eid", visitdatefield,agefield,diagfield,diagagefield)]
-      dfsub[,diagagefield][dfsub[,diagagefield] <0] <- NA
-      
+      dfsub <- df[!is.na(df[,diagfield] ) ,c("n_eid", visitdatefield,diagfield,diagagefield)]
+      dfsub[,diagagefield][dfsub[,diagagefield] <0] <- NA      
       dfsub$eventdate = dfsub[,visitdatefield] - (dfsub[,diagagefield]*daysinyear)
-      
+
       dfout <- rbind(dfout,as.matrix(dfsub[,c("n_eid","eventdate",diagfield)]))
     }
     
