7ccc08290dd94ea628b8fe059fb519411158e978
ming <mwyeung@link.cuhk.edu.hk>
2020-08-17 18:21:18 +0200

    comment

--- convert_nurseinterview_to_episodedata.r
+++ convert_nurseinterview_to_episodedata.r
@@ -30,23 +30,27 @@ convert_nurseinterview_to_episodedata <- function(df,field_sr_diagnosis = "20002
   # 
   # df = lst$df_sr # data.table
   # field_sr_diagnosis = "20002"
-  # field_sr_date = NULL # "20008"# "20008" # interpolated year
+  # field_sr_date = "20008" #NULL # "20008"# "20008" # interpolated year
   # field_sr_date_type="interpolated_year"
   # field_visit_date="53"
   # qc_treshold_year=10
   
-  # 
+  # df=dfukb
   tic(paste("convert_nurseinterview_to_episodedata: ",field_sr_diagnosis))
   
   if(!is.null(field_sr_date)) { if(field_sr_date==""){field_sr_date=NULL}} 
   if(is.null(field_sr_date))  { print("field_sr_date == NULL; qc_treshold_year and field_sr_date_type will not be used.") }
   daysinyear=365.25
   field_visit_date="53"
+  # vector with name of the identifier col 
   identifierfield = names(df)[grepl("eid", names(df))]
+  #  vector with names of all visit cols : "f.53.0.0" "f.53.1.0" "f.53.2.0" "f.53.3.0"
   visitdatefields = names(df)[grepl(paste0("[^0-9]",field_visit_date,"[^0-9]"), names(df))]
-  srdiagnosisfields = names(df)[grepl( paste0("[^0-9]",field_sr_diagnosis,"[^0-9]"), names(df))]
-  if(!is.null(field_sr_date)){srdiagnosisdatefields = names(df)[grepl(field_sr_date, names(df))]} else srdiagnosisdatefields=NULL
   
+  # default:20002, Non-cancer illness code, self-reported
+  srdiagnosisfields = names(df)[grepl( paste0("[^0-9]",field_sr_diagnosis,"[^0-9]"), names(df))]
+  # default:20008 , 	Interpolated Year when non-cancer illness first diagnosed
+  if(!is.null(field_sr_date)){srdiagnosisdatefields = names(df)[grepl(field_sr_date, names(df))]} else srdiagnosisdatefields=NULL
   visits = length(visitdatefields) #sum(grepl("53_", names(df)))
   
   # only need n_eid, visit dates and diag-codes + age-of-diag
@@ -55,6 +59,7 @@ convert_nurseinterview_to_episodedata <- function(df,field_sr_diagnosis = "20002
                       srdiagnosisfields, 
                       srdiagnosisdatefields
   )
+  # data.table - Setting with = FALSE disables the ability to refer to columns as if they are variables
   df_ <- df[,columns_to_keep,with=FALSE]
   df_$dummy <- NA
   df_out <-  matrix(ncol=5, nrow=0) # initiate output 
@@ -63,20 +68,26 @@ convert_nurseinterview_to_episodedata <- function(df,field_sr_diagnosis = "20002
     print(paste0("querying visit ",v))
     diagfields = names(df_)[grepl(paste0("[^0-9]",field_sr_diagnosis,"[^0-9]",v),names(df_))]
     if(length(diagfields)==0){print(paste0("no data on visit ",v));next}
+
     if(!is.null(field_sr_date)){diagdatefields = names(df_)[grepl(paste0("[^0-9]",field_sr_date,"[^0-9]",v),names(df_))]}
+    # f.53.v.0 
     visitdatefield = visitdatefields[v+1]
     for (i in 1:length(diagfields)) { # for each occurence of diagfield, find the corresponding age and convert it to  date - code and rbind() to df_out. 
       #agefield = paste0("age_",v)
       diagfield = diagfields[i]
+      
       if(!is.null(field_sr_date)){ 
+        # should this line go under the if else block to avoid index out of bound error?
         diagdatefield = diagdatefields[i]
         if( length(diagdatefields) == 1) { diagdatefield = diagdatefields } # in case of death (40001/2)
       } else {
           diagdatefield = "dummy" # e.g. incase of medication
           }
 
-      #print(paste0((diagfield), " - ", diagdatefield))
+      # print(paste0((diagfield), " - ", diagdatefield))
+      #  empty diagnosis column
       if(all(is.na(df_[,diagfield,with=FALSE]))){next}
+      # for rows with non-empty current diagfield, select identifier,diagfield,diagdatefield,visitdatefield 
       df_sub <- df_[!is.na(get(diagfield) ),c(identifierfield,diagfield,diagdatefield,visitdatefield),with=FALSE]
       df_sub$visit <- v
       names(df_sub) <- c(identifierfield,"code","eventdate","visitdate","visit")
@@ -86,18 +97,24 @@ convert_nurseinterview_to_episodedata <- function(df,field_sr_diagnosis = "20002
   }
   
   print("convert to dataframe")
+  # df_out contains all visits , each row in df_out is a event
   df_out <- data.table(df_out,stringsAsFactors=F)
   df_out <- df_out[, visitdate:=as.Date(visitdate)]
   #df_out <- df_out[, code:=as.character(code)] #convert_col_to_integer(df_out$code) # df_out[, code:=as.integer(code)]
+  # remove leading and trailing whitespace in column code 
   df_out[, code:=lapply(.SD, trimws), .SDcols = "code"]
+  # 99999 unclassifiable for ukb codings
   df_out <- df_out[!code %in% "99999"]
+  #  char -> integer
   df_out$code <- convert_col_to_integer(df_out$code)
   
-  
+
+  # field_sr_date <-> field_sr_date_type, default :20008 <->	Interpolated Year 
   if(!is.null(field_sr_date) & (field_sr_date_type=="interpolated_year"| field_sr_date_type=="interpolated_age" |  field_sr_date_type=="date" )) {
 
     if(field_sr_date_type=="interpolated_year") {
       df_out <- df_out[, eventdate:=as.numeric(eventdate)] ## as number. interpolated year 
+      # negative time = not meaningful. Refer coding 13
       df_out[eventdate <0,'eventdate']<-NA
       df_out$eventdate <- as.Date(convert_year_to_date(df_out$eventdate))
     } else if (field_sr_date_type=="interpolated_age"){
@@ -107,25 +124,31 @@ convert_nurseinterview_to_episodedata <- function(df,field_sr_diagnosis = "20002
     } else if (field_sr_date_type=="date"){
       df_out = df_out[, eventdate:=as.Date(eventdate)]
     }
+  
     # deduplicate, min/max/mean/sd <- not very efficient? 
     print("deduplicate")
+    # for each code in the same participant, compute min(oldest record)/max(newest record)/mean date
     dfout_extrastats<- df_out %>% group_by(!!as.name(identifierfield),code) %>%
       mutate(mindt = min(eventdate, na.rm = TRUE),maxdt = max(eventdate, na.rm = TRUE),meandt = mean(eventdate, na.rm = TRUE))
-    
+    # time between oldest and newest record in unit of year
     dfout_extrastats$diffdt <- (dfout_extrastats$maxdt - dfout_extrastats$mindt)/daysinyear
+    # if this time difference is larger than qc threshold , mark NA in meandt
     dfout_extrastats[dfout_extrastats$diffdt>qc_treshold_year ,"meandt"] <- NA
     dfout_extrastats[dfout_extrastats$diffdt > 0,]
+    #  take meandt as the event date , i.e. duplicate records with time difference > qc threshold will be changed to NA 
     df_out$eventdate <- dfout_extrastats$meandt
     df_out <- df_out[!duplicated(df_out[,c(identifierfield,"code","eventdate"),with=FALSE]),] #sorted on visit, so first occurence is always first visit. 
     
   }  else {
     df_out <- df_out[, eventdate:=as.Date(eventdate)]
   }
-  
+
   # record which can be set as an event or not (when no event_date is reported, only visit)
   df_out$event <- 1
   df_out <- df_out[, event:=as.integer(event)]
+  # mark record without valid event date with 0
   df_out[is.na(df_out$eventdate)]$event <- 0
+  # take visitdate as event date
   df_out[is.na(df_out$eventdate)]$eventdate <- df_out[is.na(df_out$eventdate)]$visitdate
   df_out <- df_out[,c(identifierfield,"code","eventdate","event"),with=FALSE]
   setkey(df_out,f.eid)    
