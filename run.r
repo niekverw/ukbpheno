b8d5d9ce089c344af8430b35d95c4b6f4adcf211
Niek V <niek@Nieks-MacBook-Pro.local>
2020-09-07 21:15:32 +0200

    add basic functoinality + diagnostic plots

--- run.r
+++ run.r
@@ -10,6 +10,7 @@ require(XML)
 require(stringr)
 library(ggplot2)
 library(ggrepel)
+library(ggpubr)
 
 if (Sys.getenv("USER")=="niek"){
   repo_dir="/Users/niek/repos/ukbpheno/"
@@ -117,7 +118,7 @@ lst.counts <- get_lst_counts(lst.data,datatable_defCol_pair = default_datatable_
 #dfukb<- dfukb[,dfhtml[dfhtml$field.showcase %in% c("eid", "53",ukb_fields$nondefault_ukb_fields),]$field.tab,with=FALSE]
 # save 
 save(dfhtml,dfukb,lst.data,lst.counts,file=fukbphenodata)
-
+# load(fukbphenodata)
 ##########################################
 #load(fukbphenodata)
 ##### I changed get_all_events() code a little bit with this  principle:
@@ -133,6 +134,7 @@ save(dfhtml,dfukb,lst.data,lst.counts,file=fukbphenodata)
 
 ##########################################
 ## analyse 1 definition
+##########################################
 dfDefinitions_processed_expanded <- expand_dfDefinitions_processed(dfDefinitions_processed,datatable_defCol_pair=default_datatable_defCol_pair(),lst.counts = lst.counts)
 all_event_dt <- get_all_events(dfDefinitions_processed_expanded[12,],lst.data) #list of 11 dfs 
 all_event_dt[, .(count = .N, sum.event = sum(event,na.rm = T),sum.epidur= sum(epidur,na.rm = T),median.epidur= median(epidur,na.rm = T),max.epidur= max(epidur,na.rm=T)), by = f.eid]
@@ -143,13 +145,89 @@ stats.codes <- all_event_dt[, .(count=.N,sum.event = sum(event,na.rm = T),sum.ep
 stats.codes <- stats.codes %>% dplyr::group_by(classification, code) %>% summarise(count=n() )
 stats.codes <- stats.codes %>% arrange(count)
 stats.codes$rank <- 1:nrow(stats.codes)
-ggplot(stats.codes, aes(rank, count,label=code,color=classification)) + geom_point() + ylim(-((max(stats.codes$count))/3),NA)  + geom_text_repel(size =3,segment.size=0.5)
-ggplot(stats.codes, aes(rank, count,label=code,color=classification)) + scale_y_continuous(trans='log2') + geom_point()   + geom_text_repel(size =3,segment.size=0.5)
+p1 <- ggplot(stats.codes, aes(rank, count,label=code,color=classification)) + geom_point() + ylim(-((max(stats.codes$count))/3),NA)  + geom_text_repel(size =3,segment.size=0.5)
+p2 <- ggplot(stats.codes, aes(rank, count,label=code,color=classification)) + scale_y_continuous(trans='log2') + geom_point()   + geom_text_repel(size =3,segment.size=0.5)
+ggarrange(p1,p2,nrow = 1, ncol = 2,common.legend = TRUE)
 
-# show co-occurences of codes 
+# show co-occurences of classifications 
+stats.coocurrence <- table(all_event_dt[,c("f.eid" ,"classification")])
+stats.coocurrence[stats.coocurrence>0] <-1
+mat <- crossprod(as.matrix(stats.coocurrence))     
+mat <- floor(t(mat * 100 / diag(mat)))                 # calculate the percentage
+pheatmap::pheatmap(mat,display_numbers=mat )
+
+# show co-occurences of codes
+stats.coocurrence <- table(all_event_dt[,c("f.eid" ,"code")])
+stats.coocurrence[stats.coocurrence>0] <-1
+mat <- crossprod(as.matrix(stats.coocurrence))     
+mat <- floor(t(mat * 100 / diag(mat)))                 # calculate the percentage
+pheatmap::pheatmap(mat,fontsize = 6)
+
+
+reshape(d,direction = 'wide',v.names='classification',idvar='f.eid',timevar='value')
+### get prevalence (move this.. )
+get_incidence_prevalence <- function(all_event_dt,reference_date) {
+  # event==0, event cannot be used forr age - of - diagnosiis or new events.
+  # event==1, event can be used for any type of future events, as it is based on ICD10 type of data 
+  # event==2, event can be used for any type of future events and age of diagnoses but only if there is no evidence in history, as it is self reported.
+  
+  # define window arround reference_dates? -- e.g. if you're diagnosed with XX, count follow-up events only after 20 days of no-events. 
+  # define window to score the diagnosis on refereence date.
+  # reference_dates # should be a vector of dates, with the identifier as name. 
+  #reference_date = setNames(as.Date(as.character(dfukb$f.53.0.0),format="%Y-%m-%d"),dfukb$f.eid)
+  
+  # 4139206
+  df.ref <- data.frame(reference_date)
+  df.ref$pid <- rownames(df.ref)
+  
+  df <- merge(all_event_dt,df.ref,by.x = 'f.eid' , by.y='pid',all.x = T) %>% arrange(eventdate) %>% as.data.table()
+  df <- df %>% filter(!is.na(reference_date)) # comment out if missing f.eids is fixed. 
+  
+  df$days <- df$eventdate - df$reference_date
+  setkey(df,days) # i don't know why, but setkey was alreaday on f.eid and cannot refresh..
+  setkey(df,f.eid)
+  
+
+  ### history
+  dfHx <- df[days<=0]
+  Hxd <- dfHx[event>0][, .(Hx_days=min(days,na.rm=T) ), keyby=list(f.eid)]
+  dfHx[,Hx:=1]
+  ### Future
+  dfFu <- df[days>0 & event==1 | (days>0 & event==2 & (!f.eid %in% dfHx$Hx) )]
+  dfFu[,Fu:=1] #unique(dfFu$f.eid)
+  Fud <- dfFu[,.(Fu_days= min(days,na.rm=T) ), keyby=list(f.eid)]
+  
+  
+  
+  ### age of diagnosis
+  #system.time({ df %>% filter(event>0) %>% group_by(f.eid) %>% summarise(first_diagnosis_days=min(days)) }) # <- slow.. 
+  #system.time({ df[df$event>0][,.(first_diagnosis_days=min(days,na.rm=T) ), by=f.eid] }) # <- fast..
+  #df[df$event>0][df[, .I[which.max(days)], by=f.eid]$V1] # <- aanother way.. 
+  first_diagnosis_days <- df[df$event>0][,.(first_diagnosis_days=min(days,na.rm=T) ), by=f.eid] # 
+  # View(Reduce(function(...) merge(..., all = TRUE,by='f.eid'), list(df,
+  #                                                                   Hxd,
+  #                                                                   Fud,
+  #                                                                   unique(dfHx[,c("f.eid","Hx")]),
+  #                                                                   unique(dfFu[,c("f.eid","Fu")]),
+  #                                                                   first_diagnosis_days)))
+
+  df <- Reduce(function(...) merge(..., all = TRUE,by='f.eid'), list( Hxd,
+                                                                Fud,
+                                                                unique(dfHx[,c("f.eid","Hx")]),
+                                                                unique(dfFu[,c("f.eid","Fu")]),
+                                                                first_diagnosis_days))
+  df[,Any:=1]
+  
+  return(df)
+}
+
+
+df <- get_incidence_prevalence(all_event_dt = all_event_dt,reference_date = setNames(as.Date(as.character(dfukb$f.53.0.0),format="%Y-%m-%d"),dfukb$f.eid))
 
 
 
+
+  
 # print(format(object.size(lst.data), units = "Mb")) #"2014.1 Mb"
 
 
