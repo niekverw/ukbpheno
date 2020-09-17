f8fba1a6640e21153ad9a7e20e74bb0d56f03603
Niek V <niek@Nieks-MacBook-Pro.local>
2020-09-17 11:16:47 +0200

    lst.data.settings

--- query-event-tables.r
+++ query-event-tables.r
@@ -6,18 +6,18 @@ to_datatype <- function(vct=c(),datatype){
 }
 
 
-get_all_events <- function (definition,lst.data=lst.data,datatable_defCol_pair=default_datatable_defCol_pair()){
+get_all_events <- function (definition,lst.data=lst.data,lst.data.settings){
   # definitions=dfDefinitions_processed_expanded[9,]
   # look up for all dataframes in list
   if(nrow(definition)>1){
     message("ERROR: Provide one definition")
     return(NULL)
   }
-  message(paste("querying the following classifications: " ,paste(names(definition)[names(definition) %in% datatable_defCol_pair$classification],collapse=", ")))
+  message(paste("querying the following classifications: " ,paste(names(definition)[names(definition) %in% lst.data.settings$classification],collapse=", ")))
   
   all_event_lst<-lapply(names(lst.data), function(x) {
-    classification=datatable_defCol_pair %>% filter(datasource == x) %>% pull(classification)
-    datatype=datatable_defCol_pair %>% filter(datasource == x) %>% pull(datatype)
+    classification=lst.data.settings %>% filter(datasource == x) %>% pull(classification)
+    datatype=lst.data.settings %>% filter(datasource == x) %>% pull(datatype)
     codes <- to_datatype(strsplit(definition[,classification],split = ",")[[1]],datatype)
     lst.data[[x]][.(codes),nomatch=NULL] # nomatch is important, otherwise it will return row with NA if it didnt find the code (but which on the other hand may also maybe good to keep track?? )
     
@@ -29,12 +29,12 @@ get_all_events <- function (definition,lst.data=lst.data,datatable_defCol_pair=d
   
   # set key to be eid
   all_event_dt <- plyr::ldply(all_event_lst, data.frame) %>% as.data.table()
-  all_event_dt$classification <- datatable_defCol_pair[match(all_event_dt$.id ,datatable_defCol_pair$datasource),]$classification
+  all_event_dt$classification <- lst.data.settings[match(all_event_dt$.id ,lst.data.settings$datasource),]$classification
   setkey(all_event_dt,f.eid)    
   return (all_event_dt)
 }
 # 
-# dfDefinitions_processed_expanded <- expand_dfDefinitions_processed(dfDefinitions_processed,datatable_defCol_pair=default_datatable_defCol_pair(),lst.counts = lst.counts)
+# dfDefinitions_processed_expanded <- expand_dfDefinitions_processed(dfDefinitions_processed,lst.data.settings=lst.data.settings(),lst.counts = lst.counts)
 # test<-get_all_events(dfDefinitions_processed_expanded[9,],lst) 
 # key(test$tte.death.icd10.secondary)
 # 
