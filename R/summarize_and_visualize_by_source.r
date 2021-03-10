17ba54841bace75e66f476fbd11bd9a07150c4bf
ming <mwyeung@link.cuhk.edu.hk>
2021-03-10 19:43:34 +0100

    remove future events in comparison

--- R/summarize_and_visualize_by_source.r
+++ R/summarize_and_visualize_by_source.r
@@ -1,7 +1,7 @@
 
 #' Get case-control status by data sources
 #'
-#' Given a phenotype, a list of episode data and reference dates per individual, identify cases and control status by source. 
+#' Given a phenotype, a list of episode data and reference dates per individual, identify cases and control status as of *reference date" by source.
 #' @param source_lst list of data sources (as vector) each containing the repsective individual sources with names matched to datasource from lst.data.setting
 #' @param definitions phenotype/trait specified in definition table (a row in the table)
 #' @param lst.data list of data table with all episode data
@@ -18,50 +18,61 @@ get_case_status_by_source <- function(source_lst,definitions,
                       lst.data,
                       lst.data.settings,
                       lst.identifiers,
-                      reference_date=NULL
+                      reference_date
                       ) {
+  lst.case_control <- get_cases_controls(definitions, lst.data,lst.data.settings,reference_date,lst.identifiers)
   ###########################################################################
   # NOTE: this function depends on the get_cases_controls! modify accordingly
   ##########################################################################
-  lst.case_control <- get_cases_controls(definitions, lst.data,lst.data.settings,reference_date,lst.identifiers)
-  
-  # extract only relevant column from df_casecontrol 
-  case_status<-lst.case_control$df.casecontrol[,c("f.eid","reference_date","Any")]
+  # extract only relevant columns from df_casecontrol
+  case_status<-lst.case_control$df.casecontrol[,c("f.eid","reference_date","Death_any","survival_days","Any","Hx")]
+
+  # ##########################################################################
+  # exclude future events
+  # otherwise the comparison between sources is not fair
+  # ##########################################################################
+  event_dt.Include_in_cases_beforeOrOn_refdate<-merge(lst.case_control$all_event_dt.Include_in_cases,case_status[,c("f.eid","reference_date")],by="f.eid")
+  # discard future events
+  message("Remove future events at reference date")
+  event_dt.Include_in_cases_beforeOrOn_refdate<-event_dt.Include_in_cases_beforeOrOn_refdate[event_dt.Include_in_cases_beforeOrOn_refdate$eventdate<=event_dt.Include_in_cases_beforeOrOn_refdate$reference_date]
+
+
   # create new columns for each source
   for (source_name in names(source_lst)) {
-    varname<-paste('Any',source_name,sep='.')
+    varname<-paste('Hx',source_name,sep='.')
     # create new columns for each source
     case_status[, (varname)]<-as.numeric(NA)
-    print(head(case_status))
-    
-    # lookup source from df with all episodes i.e. all_event_dt.Include_in_cases 
+    # discard non-event in which the event date is explicitly set to visit date? BUT some events simply have not event dates.
+    # lookup source from df with all episodes i.e. all_event_dt.Include_in_cases
     # if rows in include_in_case which originated from the target source, look up the eid and set to 2
-    case_status[[varname]][case_status$f.eid %in% lst.case_control$all_event_dt.Include_in_cases[lst.case_control$all_event_dt.Include_in_cases$.id %in% source_lst[[source_name]],]$f.eid] <- 2
-    print(head(case_status))
-    
-    # case_status$Any.sr[case_status$f.eid %in% lst.case_control$all_event_dt.Include_in_cases[lst.case_control$all_event_dt.Include_in_cases$.id %in% sr_source,]$f.eid]
+    # case_status[[varname]][case_status$f.eid %in% lst.case_control$all_event_dt.Include_in_cases[lst.case_control$all_event_dt.Include_in_cases$.id %in% source_lst[[source_name]],]$f.eid] <- 2
+    case_status[[varname]][case_status$f.eid %in% event_dt.Include_in_cases_beforeOrOn_refdate[event_dt.Include_in_cases_beforeOrOn_refdate$.id %in% source_lst[[source_name]],]$f.eid] <-2
   }
-  
-  # for all Any.x columns
-  for (j in   names(case_status)[grepl("Any",names(case_status))]){
-    # if Any <0 , copy Any (excluded)
-    set(case_status,which(case_status$Any<=0),j,case_status[case_status$Any<=0]$Any)
-    #  if the cell value was NA -> set to 1 -> control 
+  for (j in names(case_status)[grepl("Hx",names(case_status))]){
+    # for all Hx.x columns
+    # if Hx <0 , copy Hx (excluded)
+    set(case_status,which(case_status$Hx<=0),j,case_status[case_status$Hx<=0]$Hx)
+    #  if the cell value was NA -> set to 1 -> control
     set(case_status,which(is.na(case_status[[j]])),j,1)
+    message(glue::glue("Control/Case by {j}"))
 
+    print(dplyr::count(case_status,.data[[j]]))
   }
   return(case_status)
 }
+case_status<-get_case_status_by_source(source_lst=source_lst,definitions,lst.data,lst.data.settings,lst.identifiers=vct.identifiers,reference_date=setNames(as.Date(as.character(dfukb$f.53.0.0),format="%Y-%m-%d"),dfukb$f.eid))
 
-# case_status<-get_case_status_by_source(source_lst,definitions,lst.data,lst.data.settings,reference_date=setNames(as.Date(as.character(dfukb$f.53.0.0),format="%Y-%m-%d"),dfukb$f.eid))
+
+
+
 # head(case_status)
-# 
+#
 # count(case_status,Any.sr)
 # count(case_status,Any.hes)
 
 
 #TODO
-# a function to show (number of case) unique to each source 
+# a function to show (number of case) unique to each source
 
 #add radarplots
 
