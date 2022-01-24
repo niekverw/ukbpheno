55a1365bb3b6bae15f56f9e4775366522b0db5aa
mw <mingy2695@gmail.com>
2022-01-24 19:17:04 +0100

    new example codes

--- inst/workflow_example/run_example.R
+++ inst/workflow_example/run_example.R
@@ -39,27 +39,27 @@ dfData.settings <-fread(fdata_setting)
 ###################################################
 
 
-##################################################################################################################################################
-#  *******************this is an optional block -> for wiki?****************************
-##################################################################################################################################################
-# to process the definition table, we need all available codes
-# currently 2 coding systems exist for gp_clinical
-cf_read3<-dfData.settings[dfData.settings$classification=="CTV3",]$code_map
-cf_read2<-dfData.settings[dfData.settings$classification=="READ2",]$code_map
-# corresponding column names from the .txt file
-get_all_exsiting_codes(fgp_clinical,c("read_2","read_3"),c(paste0(data_dir,cf_read2),paste0(data_dir,cf_read3)))
-
-#  3 gp_script
-cf_dmd<-dfData.settings[dfData.settings$classification=="DMD",]$code_map
-# bnf.england bnf.scotland are separated because they appear to be coded slightly differently according to the official documentation...hence different post-processing may be needed
-cf_bnf<-unique(dfData.settings[dfData.settings$classification=="BNF",]$code_map)
-cf_read2d<-dfData.settings[dfData.settings$classification=="READ2_drugs",]$code_map
-get_all_exsiting_codes(fgp_scripts,c("read_2","bnf_code","dmd_code"),c(paste0(data_dir,cf_read2d),paste0(data_dir,cf_bnf),paste0(data_dir,cf_dmd)))
-
-# get_all_exsiting_codes(fhesin_diag,c('diag_icd9','diag_icd10'),c(paste0(pheno_dir,'hesin_icd9.code'),paste0(pheno_dir,'hesin_icd10.code')))
-# get_all_exsiting_codes(fdeath_cause_portal,c('cause_icd10'),c(paste0(pheno_dir,'death_icd10.code')))
-# get_all_exsiting_codes(fhesin_oper,c('oper3','oper4'),c(paste0(pheno_dir,'hesin_oper3.code'),paste0(pheno_dir,'hesin_oper4.code')))
-##################################################################################################################################################
+# ##################################################################################################################################################
+# #  *******************this is an optional block -> for wiki?****************************
+# ##################################################################################################################################################
+# # to process the definition table, we need all available codes
+# # currently 2 coding systems exist for gp_clinical
+# cf_read3<-dfData.settings[dfData.settings$classification=="CTV3",]$code_map
+# cf_read2<-dfData.settings[dfData.settings$classification=="READ2",]$code_map
+# # corresponding column names from the .txt file
+# get_all_exsiting_codes(fgp_clinical,c("read_2","read_3"),c(paste0(data_dir,cf_read2),paste0(data_dir,cf_read3)))
+#
+# #  3 gp_script
+# cf_dmd<-dfData.settings[dfData.settings$classification=="DMD",]$code_map
+# # bnf.england bnf.scotland are separated because they appear to be coded slightly differently according to the official documentation...hence different post-processing may be needed
+# cf_bnf<-unique(dfData.settings[dfData.settings$classification=="BNF",]$code_map)
+# cf_read2d<-dfData.settings[dfData.settings$classification=="READ2_drugs",]$code_map
+# get_all_exsiting_codes(fgp_scripts,c("read_2","bnf_code","dmd_code"),c(paste0(data_dir,cf_read2d),paste0(data_dir,cf_bnf),paste0(data_dir,cf_dmd)))
+#
+# # get_all_exsiting_codes(fhesin_diag,c('diag_icd9','diag_icd10'),c(paste0(pheno_dir,'hesin_icd9.code'),paste0(pheno_dir,'hesin_icd10.code')))
+# # get_all_exsiting_codes(fdeath_cause_portal,c('cause_icd10'),c(paste0(pheno_dir,'death_icd10.code')))
+# # get_all_exsiting_codes(fhesin_oper,c('oper3','oper4'),c(paste0(pheno_dir,'hesin_oper3.code'),paste0(pheno_dir,'hesin_oper4.code')))
+# ##################################################################################################################################################
 
 
 
@@ -76,16 +76,29 @@ dfDefinitions_processed_expanded<-read_defnition_table(fdefinitions,fdata_settin
 lst.harmonized.data<-harmonize_ukb_data(f.ukbtab = fukbtab,f.html = fhtml,f.gp_clinical = fgp_clinical,f.gp_scripts = fgp_scripts,f.hesin = fhesin,f.hesin_diag = fhesin_diag,f.hesin_oper =fhesin_oper,f.death_portal = fdeath_portal,f.death_cause_portal = fdeath_cause_portal )
 
 # with definition table, fields required in definition table are also included
+# rm(lst.harmonized.data)
 lst.harmonized.data<-harmonize_ukb_data(f.ukbtab = fukbtab,f.html = fhtml,dfDefinitions=dfDefinitions_processed_expanded,f.gp_clinical = fgp_clinical,f.gp_scripts = fgp_scripts,f.hesin = fhesin,f.hesin_diag = fhesin_diag,f.hesin_oper =fhesin_oper,f.death_portal = fdeath_portal,f.death_cause_portal = fdeath_cause_portal )
+
+#####################################
+# read withdrawal list and remove them from the data
+####################################
+# f_particip_withdraw<-'/mnt/THORAX_SHARE/7.UKBiobank/DataDownloads/74395/2021_06_22-47316/w74395_20210809.csv'
+
+
+f_particip_withdraw<-paste(pheno_dir,"w12345_20210809.csv",sep="")
+
+df_withdrawal<-fread(f_particip_withdraw)
 
 
 # ##################################################
 # Get case/control with an example phenotype:
 ####################################################
 # we need 1) definition of the target trait, 2)harmonized data tables, 3) data.setting dataframe and # 4) target set of individuals either specified via df_reference_date or vct.identifiers
+
+
 # 1) definition of the target trait
-trait<-"Cad"
-# dfDefinitions_processed_expanded[dfDefinitions_processed_expanded$TRAIT==trait,]$DESCRIPTION
+trait<-"DmRxT2"
+dfDefinitions_processed_expanded[dfDefinitions_processed_expanded$TRAIT==trait,]$DESCRIPTION
 # [1] "Atrial fibrillation/Atrial flutter"
 
 # 2)harmonized data table -> lst.harmonized.data
@@ -94,45 +107,93 @@ trait<-"Cad"
 # df_reference_date is a data.table with identifiers in first column and corresponding reference dates in second column
 # Time to disease will be calculated from these specified reference dates
 df_reference_dt_v0<-lst.harmonized.data$dfukb[,c("identifier","f.53.0.0")]
-df_reference_dt_v0$f.53.0.0<-as.Date(df_reference_dt_v0$f.53.0.0)
-lst.case_control <- get_cases_controls(definitions=dfDefinitions_processed_expanded %>% filter(TRAIT==trait), lst.harmonized.data$lst.data,dfData.settings, df_reference_date=df_reference_dt_v0)
+# remove the individuals who have withdrawn from the study
+df_reference_dt_v0<-df_reference_dt_v0[! identifier  %in% df_withdrawal$V1]
 
 
-df_reference_dt_v2<-lst.harmonized.data$dfukb[,c("identifier","f.53.2.0")]
-lst.case_control <- get_cases_controls(definitions=dfDefinitions_processed_expanded %>% filter(TRAIT==trait), lst.harmonized.data$lst.data,dfData.settings, df_reference_date=df_reference_dt_v2)
-# TODO CHECK numbers of cases not a sum from the messages , can be confusing
+
+# individuals with DmT2 based on self-reported codes , medication and linkage
+lst.DmRxT2.case_control <- get_cases_controls(definitions=dfDefinitions_processed_expanded %>% filter(TRAIT==trait), lst.harmonized.data$lst.data,dfData.settings, df_reference_date=df_reference_dt_v0)
 
 
-# 4.2) target set of individuals specified via vct.identifiers i.e. no specified reference dates
-# The dates of the first event will be taken as reference date for the calculation of time to disease
-lst.case_control <- get_cases_controls(definitions=dfDefinitions_processed_expanded %>% filter(TRAIT==trait), lst.harmonized.data$lst.data,dfData.settings, vct.identifiers=lst.harmonized.data$vct.identifiers)
-
-# it is a good practice to check if all the identifiers are in the main dataset, as non-existing identifier will be classified as a control (no events)
-my.curated.identifiers<-df_reference_dt_v2[1:20]$identifier
-my.curated.identifiers[1]<-10000011
-all(my.curated.identifiers %in% lst.harmonized.data$vct.identifiers) #FALSE
-
-all_event_dt <- get_all_events(definition=dfDefinitions_processed_expanded%>%filter(TRAIT==trait)%>%filter(Definitions=="Include_in_cases"),lst.data=lst.harmonized.data$lst.data,df.data.settings=dfData.settings)   #Af
-
-
-plot_individual_timeline(df.data.settings = dfData.settings,lst.data=lst.harmonized.data$lst.data,ind_identifier = 1111111)
-
+View(lst.DmRxT2.case_control$all_event_dt.Include_in_cases.summary)
+View(lst.DmRxT2.case_control$df.casecontrol)
+# get a disease time line to see the relative contribution of different data sources over time
 DmRxT2_timeline<-plot_disease_timeline_by_source(definition=dfDefinitions_processed_expanded%>%filter(TRAIT==trait),lst.harmonized.data$lst.data,dfData.settings,lst.harmonized.data$vct.identifiers)
 DmRxT2_timeline
+# get a UpSet plot to see the relative contribution of different data sources at baseline
+upset_plot<-make_upsetplot(definition=dfDefinitions_processed_expanded%>%filter(TRAIT==trait),lst.harmonized.data$lst.data,dfData.settings,df.reference.dates = df_reference_dt_v0)
+upset_plot
+# extract all hospital admission records
+all_DmRxT2_evnt<-lst.DmRxT2.case_control$all_event_dt.Include_in_cases
+DmRxT2_hesin_rec<-all_DmRxT2_evnt[grepl ("hesin" ,all_DmRxT2_evnt$.id)]
+#  get some descriptive statistics on the records on a code level
+hesin_stats<-get_stats_for_events(DmRxT2_hesin_rec)
+hesin_stats$stats.codes.summary.p
+
+#  get some summary statistics on the records on individual level
+DmRxT2_rec_cnt<-DmRxT2_hesin_rec[,.(count=.N),by=c("identifier")]
+max(DmRxT2_rec_cnt$count)
+median(DmRxT2_rec_cnt$count)
+mean(DmRxT2_rec_cnt$count)
+quantile(DmRxT2_rec_cnt$count)
+# visualization count with barplot
+ggplot2::ggplot(DmRxT2_rec_cnt, ggplot2::aes(x=count)) +
+  ggplot2::geom_bar(fill="#0073C2FF" )
+
+# plot individual time line
+plot_individual_timeline(df.data.settings = dfData.settings,lst.data=lst.harmonized.data$lst.data,ind_identifier = 1111111)
+
+################################
+# refine the diagnoses
+###############################
+# identify individuals with specific DmT2 codes
+lst.DmT2.case_control<-get_cases_controls(definitions=dfDefinitions_processed_expanded %>% filter(TRAIT=="DmT2"), lst.harmonized.data$lst.data,dfData.settings, df_reference_date=df_reference_dt_v0)
+# identify individuals with specific DmT1 codes
+lst.DmT1.case_control<-get_cases_controls(definitions=dfDefinitions_processed_expanded %>% filter(TRAIT=="DmT1"), lst.harmonized.data$lst.data,dfData.settings, df_reference_date=df_reference_dt_v0)
+# identify individuals with DmG
+lst.DmG.case_control <- get_cases_controls(definitions=dfDefinitions_processed_expanded %>% filter(TRAIT=="DmG"), lst.harmonized.data$lst.data,dfData.settings, df_reference_date=df_reference_dt_v0)
+# identify individuals with metformin use , 1140884600 contributes 15,085
+lst.RxMet.case_control <- get_cases_controls(definitions=dfDefinitions_processed_expanded %>% filter(TRAIT=="RxMet"), lst.harmonized.data$lst.data,dfData.settings, df_reference_date=df_reference_dt_v0)
+# identify indviduals with diagnosis codes related diabetes excluding medication use
+lst.Dm.case_control <- get_cases_controls(definitions=dfDefinitions_processed_expanded %>% filter(TRAIT=="Dm"), lst.harmonized.data$lst.data,dfData.settings, df_reference_date=df_reference_dt_v0)
+# identify young onset self reported diabetes (european origin)
+lst.SrDmYEw.case_control <- get_cases_controls(definitions=dfDefinitions_processed_expanded %>% filter(TRAIT=="SrDmYEw"), lst.harmonized.data$lst.data,dfData.settings, df_reference_date=df_reference_dt_v0)
+# identify young onset self reported diabetes (carribean african origin)
+lst.SrDmYSaCa.case_control <- get_cases_controls(definitions=dfDefinitions_processed_expanded %>% filter(TRAIT=="SrDmYSaCa"), lst.harmonized.data$lst.data,dfData.settings, df_reference_date=df_reference_dt_v0)
+#identify use of insulin/oral diabetic medication other than metformin
+lst.RxDmNoMet.case_control <- get_cases_controls(definitions=dfDefinitions_processed_expanded %>% filter(TRAIT=="RxDmNoMet"), lst.harmonized.data$lst.data,dfData.settings, df_reference_date=df_reference_dt_v0)
+#identify individuals that are on metformin but no self-reported diabetes nor use of insulin/oral diabetic medication other than metformin
+RxMet_DmUnlikely<-setdiff(lst.RxMet.case_control$all_event_dt.Include_in_cases$identifier,union(lst.Dm.case_control$all_event_dt.Include_in_cases.summary$identifier,lst.RxDmNoMet.case_control$all_event_dt.Include_in_cases$identifier))
+
+# identify individuals with self-report insulin <12 months post-diagnosis
+lst.RxDmInsFirstYear.case_control<-get_cases_controls(definitions=dfDefinitions_processed_expanded %>% filter(TRAIT=="RxDmInsFirstYear"), lst.harmonized.data$lst.data,dfData.settings, df_reference_date=df_reference_dt_v0)
+#identify individuals with insulin
+lst.RxDmIns.case_control<-get_cases_controls(definitions=dfDefinitions_processed_expanded %>% filter(TRAIT=="RxDmIns"), lst.harmonized.data$lst.data,dfData.settings, df_reference_date=df_reference_dt_v0)
+
+# cross examine various diagnoses, for example to get young onset diabetes who do not have evidence of  other types of diabetes (on insulin, on insulin within 1 year of diagnosis, specific type 1 diabetes codes, specific gestational diabetes codes)
+# individuals of young onset diabetes
+ind_young_onset<- union(lst.SrDmYSaCa.case_control$all_event_dt.Include_in_cases$identifier,lst.SrDmYEw.case_control$all_event_dt.Include_in_cases$identifier)
+# individuals with evidence of other types of diabetes reported
+ind_RxIns_DmT1_DmG<- union(union(lst.DmT1.case_control$all_event_dt.Include_in_cases$identifier,lst.SrDmYEw.case_control$all_event_dt.Include_in_cases$identifier),lst.DmG.case_control$all_event_dt.Include_in_cases$identifier)
+
+inds_young_onset_probable_DmT2 <-setdiff(ind_young_onset,ind_RxIns_DmT1_DmG)
+# remove individuals who are on the metformin but likely NOT diabetes
+inds_young_onset_probable_DmT2 <-setdiff(inds_young_onset_probable_DmT2,RxMet_DmUnlikely)
+
+# check if these individuals have specific DmT2 codes
+# and inspect the data on those individuals who don't  i.e. the uncertain cases
+lst.DmRxT2.case_control$all_event_dt.Include_in_cases[identifier %in% setdiff(inds_young_onset_probable_DmT2,lst.DmT2.case_control$all_event_dt.Include_in_cases$identifier)]
 
 
-# TODO check the Hx part is not working
-DmRxT2_status_by_source<-get_case_status_by_source(definition=dfDefinitions_processed_expanded%>%filter(TRAIT==trait),lst.harmonized.data$lst.data,df.data.settings,df.reference.dates = df_reference_dt_v0)
-df_reference_dt_today$f.53.0.0<-Sys.Date()
-DmRxT2_today_status_by_source<-get_case_status_by_source(definition=dfDefinitions_processed_expanded%>%filter(TRAIT==trait),lst.harmonized.data$lst.data,df.data.settings,df.reference.dates = df_reference_dt_today)
-
-
-Cad_status_by_source<-get_case_status_by_source(definition=dfDefinitions_processed_expanded%>%filter(TRAIT==trait),lst.harmonized.data$lst.data,dfData.settings,df.reference.dates = df_reference_dt_v0)
-Cad_status_by_source$Cad_gp<-0
-# Cad_status_by_source$Cad_gp[Cad_status_by_source$<-0
 
 
 
 
-Cad_upset<-make_upsetplot(definition=dfDefinitions_processed_expanded%>%filter(TRAIT==trait),lst.harmonized.data$lst.data,dfData.settings,df.reference.dates = df_reference_dt_v0)
-Cad_upset
+
+
+
+
+
+
+
